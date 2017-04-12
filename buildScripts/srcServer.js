e7273c2779b792cbd10d2a9958ff58582f0835e2
Reedina <reedinafarms@gmail.com>
2017-04-11 18:02:20 -0700

    updates

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,11 +1,11 @@
-var expressn = require('express');
+var express = require('express');
 var path = require('path');
 var open = require('open');
 
 var port = 3000;
 var app = express();
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, '../src/index.html'));hh
+  res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
 app.listen(port, function(err){
