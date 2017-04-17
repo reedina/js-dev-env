c8fdb4573b7035d7ad9d11d81000627c41588a29
Reedina <reedinafarms@gmail.com>
2017-04-17 07:26:02 -0700

    added linting

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,2 +1,4 @@
 import chalk from 'chalk';
-console.log(chalk.green('Starting app in dev mode...')); // eslint-disable no-console
+
+/*eslint no-console: ["error", { allow: ["warn", "error"] }] */
+console.log(chalk.green('Starting app in dev mode...')); // eslint-disable-line no-console
--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0.00');
 
-console.log(`I would pay ${courseValue} for this awesome course!`);
+/*eslint no-console: ["error", { allow: ["warn", "error"] }] */
+console.log(`I would pay ${courseValue} for this awesome course!`); //eslint-disable-line no-console
 
