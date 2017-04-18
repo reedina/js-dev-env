2883d5528dfefb228d7fa146c5a96862776ff9c9
Reedina <reedinafarms@gmail.com>
2017-04-18 13:41:45 -0700

    added mock data features

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './index.css';
-import {getUsers} from './api/userApi';
+import {getUsers, deleteUser} from './api/userApi';
 
 // Populate table of users via API call
 getUsers().then(result => {
@@ -15,5 +15,17 @@ getUsers().then(result => {
         </tr>`
   });
   global.document.getElementById('users').innerHTML = usersBody;
-});
 
+const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+
+Array.from(deleteLinks, link =>{
+   link.onClick = function(event) {
+    const element = event.target;
+    event.preventDefault();
+    deleteUser(element.attributes["data-id"].value);
+    const row = element.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+   };
+  });
+});
